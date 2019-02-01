6499934a3bc419a78217aacac3ec603f01ea1276
xjh <xjh@localhost.xjh>
2019-02-01 19:57:33 +0800

    add mystrcpy code

---
+++ mystrcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+char* My_Strcpy(char* dest,const char* src)
+{
+  assert(dest);
+  assert(src);
+  while(*dest++ = *src++)
+  {
+
+  }
+}
+int main()
+{
+  char arr[10]={'0'};
+  My_Strcpy(arr,"mystrcpy");
+  printf("%s\n",arr);
+  return 0;
+}
